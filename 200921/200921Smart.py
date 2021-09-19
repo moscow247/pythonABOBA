2ec7a4c0fe733314e5110cc114a7e7b331c12360
Alexey <50459955+moscow247@users.noreply.github.com>
2021-09-20 00:17:16 +0300

    Create 200921Smart.py

---
+++ 200921/200921Smart.py
@@ -0,0 +1,16 @@
+a=float(input())
+b=float(input())
+c=float(input())
+sumCc=0
+mainCc=0
+if(a+b>c and a+c>b and b+c>a):
+    sumCc=((a+b+c)-max(a,b,c)-min(a,b,c))**2 + min(a,b,c)**2
+    mainCc=max(a,b,c)**2
+    if(mainCc==sumCc):
+        print("Прямоугольный, остроугольный")
+    if(mainCc>sumCc):
+        print("Равносторонний" if a==b and b==c and a==c else ("Равнобедренный" if a==b or b==c or a==c else "просто остроугольный"))
+    if(mainCc<sumCc):
+        print("Равнобедренный" if a==b or a==c or b==c else "просто тупоугольный")
+else:
+    print("Треугольника с такими сторонами не существует")
