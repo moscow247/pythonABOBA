7c52f537d08715204f0f0e5ac13e6b9a552fdc9e
Alexey <50459955+moscow247@users.noreply.github.com>
2021-09-20 00:20:33 +0300

    Update 200921.py

--- 200921/200921.py
+++ 200921/200921.py
@@ -1,21 +1,24 @@
-a=int(input())
-b=int(input())
-c=int(input())
+a=float(input())
+b=float(input())
+c=float(input())
 if(a+b>c and a+c>b and b+c>a):
     if(max(a,b,c)**2==((a+b+c)-min(a,b,c)-max(a,b,c))**2 + min(a,b,c)**2):
         print("Прямоугольный, остроугольный")
     if(max(a,b,c)**2>((a+b+c)-min(a,b,c) -max(a,b,c))**2 + min(a,b,c)**2):
         if(a==b and b==c and a==c):
             print("Равносторонний")
-        else: 
-            if (a == b | b == c | a == c):
+        else:
+            if (a == b or b == c or a == c):
                 print("Равнобедренный")
             else:
                 print("просто остроугольный")
     if(max(a,b,c)**2<((a+b+c)-min(a,b,c)-max(a,b,c))**2 + min(a,b,c)**2):
-        if(a==b | a==c| b==c):
+        if(a==b or a==c or b==c):
             print("Равнобедренный")
-        else: 
+        else:
             print("просто тупоугольный")
 else:
     print("Треугольника с такими сторонами не существует")
+
+
+ # lol 
