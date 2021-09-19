8e4956cfaf93b643b32da7a6c9bdecb7cc014343
Alexey <50459955+moscow247@users.noreply.github.com>
2021-09-20 00:23:05 +0300

    устранил неполадки логики)

--- 200921/200921.py
+++ 200921/200921.py
@@ -6,15 +6,15 @@ if(a+b>c and a+c>b and b+c>a):
         print("Прямоугольный, остроугольный")
     if(max(a,b,c)**2>((a+b+c)-min(a,b,c) -max(a,b,c))**2 + min(a,b,c)**2):
         if(a==b and b==c and a==c):
-            print("Равносторонний")
+            print("Равносторонний, остроугольный")
         else:
             if (a == b or b == c or a == c):
-                print("Равнобедренный")
+                print("Равнобедренный, остроугольный")
             else:
                 print("просто остроугольный")
     if(max(a,b,c)**2<((a+b+c)-min(a,b,c)-max(a,b,c))**2 + min(a,b,c)**2):
         if(a==b or a==c or b==c):
-            print("Равнобедренный")
+            print("Равнобедренный, тупоугольный")
         else:
             print("просто тупоугольный")
 else:
